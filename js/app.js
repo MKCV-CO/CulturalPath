c533be83d95e597fbbff9d63097a33981696e3f5
VINICIUSNUNES137 <90266473+VINICIUSNUNES137@users.noreply.github.com>
2023-06-14 15:14:25 -0300

    refactor: texts > lorem

--- js/app.js
+++ js/app.js
@@ -2,12 +2,3 @@
 import { alterColor } from './header-footer.js';
 
 import './router.js'
-
-document.addEventListener('DOMContentLoaded', function () {
-  const navbarCheckbox = document.getElementById('navbar__checkbox');
-  const navbarCloseButton = document.querySelector('.navbar__close');
-
-  navbarCloseButton.addEventListener('click', function () {
-    navbarCheckbox.checked = false;
-  });
-});
--- js/header-footer.js
+++ js/header-footer.js
@@ -1,15 +1,6 @@
 // ALTERAÇÃO DA COR DO BOTÃO DOE
 export const alterColor = (color) => {
     document.getElementById("button_doe").style.backgroundColor = color
-    document.getElementById("button-footer").style.backgroundColor = color
+    document.getElementById("button_doe-oculto").style.backgroundColor = color
 }
-
-// NAVBAR
-// let menu = document.querySelector('#menu-icon')
-// let navbar = document.querySelector('.navbar')
-
-// menu.onclick = () => {
-//     menu.classList.toggle('bx-x')
-//     navbar.classList.toggle('open')
-// }
 
